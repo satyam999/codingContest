d67330bed83b52b3aa62884f02d9e8b72aadd2fc
satyam999 <durgeshnandani999@gmail.com>
2018-06-22 01:22:58 +0530

    Initial chekin for proj2 commit

---
+++ Proj2/src/dops/Class1.java
@@ -0,0 +1,11 @@
+package dops;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First project with github");
+
+	}
+
+}
