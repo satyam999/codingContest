20dfe7355661c1f74dda19d7aa8600dc07d4e7c5
satyam999 <durgeshnandani999@gmail.com>
2018-06-27 07:45:13 +0530

    latest commit late

--- Proj2/src/dops/Class1.java
+++ Proj2/src/dops/Class1.java
@@ -4,8 +4,9 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First project with github");
-
+		System.out.println("First project with github"); 
+		System.out.println("First project with github Line 2");
+		
 	}
 
 }
